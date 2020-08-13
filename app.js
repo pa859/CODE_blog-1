fa71f9efe61d80f936477e3fee871b6cf6295ce7
Shiv9235 <shivraj9235upadhyay@gmail.com>
2020-08-13 14:18:34 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,8 @@
+var bodyParser     = require("body-parser"),
+	mongoose       = require("mongoose"),
+	methodOverride = require("method-override"),
+	sanitizer  	   = require("express-sanitizer"),
+	app		       = require("express")();
+
+
+//server require
